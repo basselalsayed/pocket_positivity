0141e19c89991a888f79eb746d80394a5cb57d0e
David Papamichael <davidpapamichael@gmail.com>
2020-06-02 11:42:53 +0100

    line chart rendered in functional component on own page of app

--- screens/MoodChart.js
+++ screens/MoodChart.js
@@ -1,53 +1,75 @@
 import React from 'react';
-import LineChart from 'react-native-chart-kit';
+import { LineChart } from 'react-native-chart-kit';
+import { Text, View, Dimensions } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
+import {
+  Svg,
+  Circle,
+  Polygon,
+  Polyline,
+  Path,
+  Rect,
+  G,
+} from 'react-native-svg';
 
-export default function MoodChart() {
+const MoodChart = () => {
+  // const height = 220;
+  // const ;
+  // const chartConfig = {
+  //   backgroundGradientFrom: '#1E2923',
+  //   backgroundGradientFromOpacity: 0,
+  //   backgroundGradientTo: '#08130D',
+  //   backgroundGradientToOpacity: 0.5,
+  //   color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
+  //   strokeWidth: 2, // optional, default 3
+  //   barPercentage: 0.5,
+  //   useShadowColorFromDataset: false, // optional
+  // };
+
   return (
     <View>
-      <Text>Bezier Line Chart</Text>
-      <LineChart
-        data={{
-          labels: ['January', 'February', 'March', 'April', 'May', 'June'],
-          datasets: [
-            {
-              data: [
-                Math.random() * 100,
-                Math.random() * 100,
-                Math.random() * 100,
-                Math.random() * 100,
-                Math.random() * 100,
-                Math.random() * 100,
-              ],
+      <ScrollView>
+        <Text>Bezier Line Chart</Text>
+        <LineChart
+          data={{
+            labels: ['January', 'February', 'March', 'April', 'May', 'June'],
+            datasets: [
+              {
+                data: [20, 45, 28, 80, 99, 43],
+                strokeWidth: 2, // optional
+              },
+            ],
+          }}
+          width={Dimensions.get('window').width} // from react-native
+          height={220}
+          yAxisLabel="$"
+          yAxisSuffix="k"
+          yAxisInterval={1} // optional, defaults to 1
+          chartConfig={{
+            backgroundColor: '#e26a00',
+            backgroundGradientFrom: '#fb8c00',
+            backgroundGradientTo: '#ffa726',
+            decimalPlaces: 2, // optional, defaults to 2dp
+            color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+            labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+            style: {
+              borderRadius: 16,
             },
-          ],
-        }}
-        width={Dimensions.get('window').width} // from react-native
-        height={220}
-        yAxisLabel="$"
-        yAxisSuffix="k"
-        yAxisInterval={1} // optional, defaults to 1
-        chartConfig={{
-          backgroundColor: '#e26a00',
-          backgroundGradientFrom: '#fb8c00',
-          backgroundGradientTo: '#ffa726',
-          decimalPlaces: 2, // optional, defaults to 2dp
-          color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-          labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-          style: {
+            propsForDots: {
+              r: '6',
+              strokeWidth: '2',
+              stroke: '#ffa726',
+            },
+          }}
+          bezier
+          style={{
+            marginVertical: 8,
             borderRadius: 16,
-          },
-          propsForDots: {
-            r: '6',
-            strokeWidth: '2',
-            stroke: '#ffa726',
-          },
-        }}
-        bezier
-        style={{
-          marginVertical: 8,
-          borderRadius: 16,
-        }}
-      />
+          }}
+        />
+      </ScrollView>
     </View>
   );
-}
+};
+
+export default MoodChart;
