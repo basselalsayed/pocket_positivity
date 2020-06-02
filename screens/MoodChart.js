62ea2538bd1644a2e6e14f56404bc977bc3c2c18
David Papamichael <davidpapamichael@gmail.com>
2020-06-02 12:25:40 +0100

    chart rendered with data from the database - scores for user 1

--- screens/MoodChart.js
+++ screens/MoodChart.js
@@ -6,60 +6,82 @@ import axios from 'axios';
 
 const MoodChart = () => {
   const [data, setData] = useState([]);
+  const [labels, setLabels] = useState([]);
 
   useEffect(() => {
     axios
       .get('https://help-for-heroes.herokuapp.com/scores/1')
-      .then((response) => setData(response.data))
+      .then((response) => getScores(response.data))
       .catch((error) => {
         console.error(error);
       });
-  });
+  }, []);
 
-  return (
-    <View>
-      <ScrollView>
-        <Text>Bezier Line Chart</Text>
-        <LineChart
-          data={{
-            labels: ['January', 'February', 'March', 'April', 'May', 'June'],
-            datasets: [
-              {
-                data: [20, 45, 28, 80, 99, 43],
-                strokeWidth: 2, // optional
-              },
-            ],
-          }}
-          width={Dimensions.get('window').width} // from react-native
-          height={220}
-          yAxisLabel="$"
-          yAxisSuffix="k"
-          yAxisInterval={1} // optional, defaults to 1
-          chartConfig={{
-            backgroundColor: '#e26a00',
-            backgroundGradientFrom: '#fb8c00',
-            backgroundGradientTo: '#ffa726',
-            decimalPlaces: 2, // optional, defaults to 2dp
-            color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-            labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-            style: {
-              borderRadius: 16,
-            },
-            propsForDots: {
-              r: '6',
-              strokeWidth: '2',
-              stroke: '#ffa726',
-            },
-          }}
-          bezier
-          style={{
-            marginVertical: 8,
-            borderRadius: 16,
-          }}
-        />
-      </ScrollView>
-    </View>
-  );
+  const getScores = (response) => {
+    let dataArray = [];
+    let labelsArray = [];
+    response.forEach((element) => {
+      dataArray.push(element.score);
+      labelsArray.push(element.date.slice(0, 10));
+    });
+    setData(dataArray);
+    setLabels(labelsArray);
+  };
+
+  const renderChart = () => {
+    if (data.length === 0) {
+      return <Text>Loading ...</Text>;
+    } else {
+      return (
+        <View>
+          {console.log(data)}
+          <ScrollView>
+            <Text>Bezier Line Chart</Text>
+            <LineChart
+              data={{
+                labels: labels,
+                datasets: [
+                  {
+                    data: data,
+                    strokeWidth: 2, // optional
+                  },
+                ],
+              }}
+              width={Dimensions.get('window').width} // from react-native
+              height={220}
+              // yAxisLabel="$"
+              // yAxisSuffix="k"
+              yAxisInterval={1} // optional, defaults to 1
+              chartConfig={{
+                backgroundColor: '#0000A0',
+                backgroundGradientFrom: '#fb8c00',
+                backgroundGradientTo: '#ffa726',
+                decimalPlaces: 2, // optional, defaults to 2dp
+                color: (opacity = 0.5) => `rgba(244, 250, 211, ${opacity})`,
+                labelColor: (opacity = 0.5) =>
+                  `rgba(156, 146, 232, ${opacity})`,
+                style: {
+                  borderRadius: 16,
+                },
+                propsForDots: {
+                  r: '6',
+                  strokeWidth: '2',
+                  stroke: '#ffa726',
+                },
+              }}
+              bezier
+              style={{
+                marginVertical: 8,
+                borderRadius: 16,
+              }}
+            />
+          </ScrollView>
+        </View>
+      );
+    }
+  };
+
+  return renderChart();
 };
 
 export default MoodChart;
